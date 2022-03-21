ea16159cb28c657b5cd29103c45af8f2296f603e
koval <88kg@mail.ru>
2022-03-21 22:53:00 +0300

    migration DATE_ROUND_DOWN

--- database/migrations/2022_03_21_000001_function_date_round_down.php
+++ database/migrations/2022_03_21_000001_function_date_round_down.php
@@ -7,15 +7,25 @@ class FunctionDateRoundDown extends Migration
 {
     public function up()
     {
-        $sql = '
+        $sql = <<<SQL
 CREATE OR REPLACE FUNCTION DATE_ROUND_DOWN(base_date timestamptz, round_interval INTERVAL)
-    RETURNS timestamptz AS
-$BODY$
-SELECT TO_TIMESTAMP(EXTRACT(epoch FROM date_trunc(\'hour\', $1))::INTEGER +
-                    trunc((EXTRACT(epoch FROM $1)::INTEGER - EXTRACT(epoch FROM date_trunc(\'hour\', $1))::INTEGER) /
-                          EXTRACT(epoch FROM $2)::INTEGER) * EXTRACT(epoch FROM $2)::INTEGER)
-$BODY$ LANGUAGE SQL STABLE;
-';
+    RETURNS timestamptz
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    result timestamptz;
+BEGIN
+    result = (SELECT TO_TIMESTAMP(EXTRACT(epoch FROM date_trunc('hour', $1))::INTEGER +
+                                  trunc((EXTRACT(epoch FROM $1)::INTEGER -
+                                         EXTRACT(epoch FROM date_trunc('hour', $1))::INTEGER) /
+                                        EXTRACT(epoch FROM $2)::INTEGER) * EXTRACT(epoch FROM $2)::INTEGER) as result);
+
+    RETURN result;
+END;
+$$;
+SQL;
+
         DB::unprepared($sql);
     }
 
